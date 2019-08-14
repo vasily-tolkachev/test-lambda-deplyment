57a9845c807e748d4ab6ec55ee03d042d3ecd55e
Vasilii Tolkachev <vasilii_tolkachev@epam.com>
2019-08-14 19:59:42 +0300

    Second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda upladed from repository!'),
+        body: JSON.stringify('Hello from Lambda upladed from repository2'),
     };
     return response;
 };

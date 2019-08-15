875b44eabbfda78bdce9439ec86d170ddf5faa0e
Vasilii Tolkachev <vasilii_tolkachev@epam.com>
2019-08-15 18:00:43 +0300

    Next

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Test checking out from git using pipline1'),
+        body: JSON.stringify('Test checking out from git using pipline2'),
     };
     return response;
 };

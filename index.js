033588f5dbd4ad085e56247d149708801e23160f
Vasilii Tolkachev <vasilii_tolkachev@epam.com>
2019-08-15 17:45:44 +0300

    Test update watching

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda upladed from repository2'),
+        body: JSON.stringify('Test checking out from git using pipline'),
     };
     return response;
 };
